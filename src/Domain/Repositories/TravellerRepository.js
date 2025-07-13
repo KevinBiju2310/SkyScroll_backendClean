d2d006e6c1ea5c272bdcc7afd17b101efb7853d7
Kevin <kevinjamesbiju2000@gmail.com>
2025-07-13 13:59:58 +0530

    changes made to travellerRepo

--- src/Domain/Repositories/TravellerRepository.js
+++ src/Domain/Repositories/TravellerRepository.js
@@ -1,3 +1,15 @@
-class TravellerRepository {}
+class TravellerRepository {
+  findAll(id) {
+    throw new Error("method findAll() not implemented");
+  }
+
+  findByPassportNumber(passportNumber) {
+    throw new Error("method findByPassportNumber() not implemented");
+  }
+
+  createTraveller(travellerData) {
+    throw new Error("method createTraveller() not implemented");
+  }
+}
 
 module.exports = TravellerRepository;
